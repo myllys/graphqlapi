48ce8f30776084af7b3080d8e6c692543c61788c
myllys <saanteri.myllys@gmail.com>
2022-03-14 09:31:30 +0200

    valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function App() {
+  const LIST_COUNTRIES = gql`
+    {
+      countries {
+        name
+        code
+        continent{
+          code
+          name
+        } 
+    }
+    }
+  `;
+  const [country, setCountry] = useState('US');
+  const { loading, error, data } = useQuery(LIST_COUNTRIES);
+
+if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      {data.countries.map(country => (
+        <tr key={country}>
+        <td> Code: {country.code} </td>
+        <td> Country: {country.name} </td>
+        <td> Continent: {country.continent.name} </td>
+        </tr>
+      ))}
+    </table>
   );
 }
-
 export default App;
